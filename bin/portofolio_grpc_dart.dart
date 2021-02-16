9b6a1ad87c8397959584f6ff9bb0075529cbebfc
acmadi <diahmad330@gmail.com>
2021-02-16 10:26:06 +0700

    init code

---
+++ bin/portofolio_grpc_dart.dart
@@ -0,0 +1,5 @@
+import 'package:portofolio_grpc_dart/portofolio_grpc_dart.dart' as portofolio_grpc_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${portofolio_grpc_dart.calculate()}!');
+}
---
+++ test/portofolio_grpc_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:portofolio_grpc_dart/portofolio_grpc_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
